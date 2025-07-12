bd8e0e90f278c3b466560cfd8663c61c8343f896
efuj <59378750+efuj@users.noreply.github.com>
2025-07-13 00:52:03 +0800

    Delete nctv.php
    y

--- nctv.php
+++
@@ -1,6 +0,0 @@
-﻿<?php
-$id = $_REQUEST['id'];
-$ids = array("南充综合"=>"7","南充公共"=>"8","南充科教"=>"9",);
-$url='https://file.ncntv.com.cn/vms/videos/nmip-media/channellive/channel'.$ids[$id].'/playlist.m3u8';
-header('location:'.urldecode($url));
-?>
